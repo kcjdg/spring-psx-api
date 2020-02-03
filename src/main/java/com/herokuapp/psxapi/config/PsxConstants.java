a810d701a6e512b120ca45ed3c51cedd90482825
Ken Crisjohn de Guzman <ken@sinonet.ph>
2020-02-03 17:22:25 +0800

    Move methods PsxConstant

--- src/main/java/com/herokuapp/psxapi/config/PsxConstants.java
+++ src/main/java/com/herokuapp/psxapi/config/PsxConstants.java
@@ -4,4 +4,5 @@ public class PsxConstants {
     private final static String HOME = "home.html";
     private final static String COMPANY = "companyInfo.html";
     public final static String CACHE_STOCKS = "ALL_STOCKS";
+    public final static String PSEI_TICKER_METHOD = "getSecuritiesAndIndicesForPublic";
 }
