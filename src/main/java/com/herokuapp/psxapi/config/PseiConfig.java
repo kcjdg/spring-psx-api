c472f70e991e3e0fa8b2cfd506c42ace052b5c9b
Ken Crisjohn de Guzman <ken@sinonet.ph>
2020-02-15 00:15:11 +0800

    firebase config

--- src/main/java/com/herokuapp/psxapi/config/PseiConfig.java
+++ src/main/java/com/herokuapp/psxapi/config/PseiConfig.java
@@ -2,11 +2,15 @@ package com.herokuapp.psxapi.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Map;
 
 
@@ -21,6 +25,8 @@ public class PseiConfig {
     private String stocksTickerApiName;
     private String stocksCompanyApiName;
     private String companyPriceApiName;
+    private String firebaseToken;
+    private String firebaseApi;
 
     @PostConstruct
     public void setAPIUrls() {
@@ -33,8 +39,33 @@ public class PseiConfig {
             this.stocksTickerApiName = map.get("get_stocks_api");
             this.stocksCompanyApiName = map.get("get_companyInfo_api");
             this.companyPriceApiName = map.get("get_companyPrice_api");
+            this.firebaseApi = map.get("firebase_api");
         } catch (Exception e) {
-            log.info("Unable to set the json config");
+            e.printStackTrace();
+            log.info("Unable to set the json config " +e.getStackTrace());
+        }
+    }
+
+
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void refreshFirebaseAuth(){
+        try {
+            FileInputStream serviceAccount = new FileInputStream("firebaseauth.json");
+            // Authenticate a Google credential with the service account
+            GoogleCredential googleCred = GoogleCredential.fromStream(serviceAccount);
+            // Add the required scopes to the Google credential
+            GoogleCredential scoped = googleCred.createScoped(
+                    Arrays.asList(
+                            "https://www.googleapis.com/auth/firebase.database",
+                            "https://www.googleapis.com/auth/userinfo.email"
+                    )
+            );
+            // Use the Google credential to generate an access token
+            scoped.refreshToken();
+            firebaseToken = scoped.getAccessToken();
+            log.info("generated new token.. {}", firebaseToken);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
